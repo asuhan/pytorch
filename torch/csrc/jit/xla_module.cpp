3221425394f8f197ccd0a38db6764173740cad63
Alex Şuhan <asuhan@google.com>
2018-06-15 16:13:23 -0700

    Append parameters to the inputs of the XLA computation

--- torch/csrc/jit/xla_module.cpp
+++ torch/csrc/jit/xla_module.cpp
@@ -3,7 +3,11 @@
 namespace torch { namespace jit {
 
 at::Tensor XlaModule::run(const std::vector<at::Tensor>& inputs) {
-  const auto maybe_tensor = xla_code_.run(inputs);
+  std::vector<at::Tensor> inputs_and_parameters(inputs.begin(), inputs.end());
+  inputs_and_parameters.insert(inputs_and_parameters.end(),
+                               model_parameters_.begin(),
+                               model_parameters_.end());
+  const auto maybe_tensor = xla_code_.run(inputs_and_parameters);
   if (!maybe_tensor.has_value()) {
     throw std::runtime_error("Failed to run XLA module");
   }
