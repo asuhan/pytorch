12a3b87abae0fe210c2a29f02252fe53a19b326e
Alex Şuhan <alex.suhan@gmail.com>
2018-06-16 16:36:01 -0700

    Collect model parameters recursively

--- torch/csrc/jit/passes/xla.cpp
+++ torch/csrc/jit/passes/xla.cpp
@@ -2,13 +2,26 @@
 
 namespace torch { namespace jit {
 
+namespace {
+
+static void gather_parameters(
+    std::vector<at::Tensor>& model_parameters,
+    const script::Module& module) {
+  for (auto& param : module.get_parameters()) {
+    model_parameters.push_back(*param->slot());
+  }
+  for (const auto& sub : module.get_modules()) {
+    gather_parameters(model_parameters, *sub->module);
+  }
+}
+
+} // namespace
+
 std::shared_ptr<XlaModule> ToXLA(script::Module& module) {
   const auto method = module.find_method("forward");
   assert(method);
   std::vector<at::Tensor> model_parameters;
-  for (const auto& model_parameter_it : module.get_parameters()) {
-    model_parameters.push_back(*(model_parameter_it->slot()));
-  }
+  gather_parameters(model_parameters, module);
   return std::make_shared<XlaModule>(method, model_parameters);
 }
 
