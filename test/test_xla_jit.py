a4fcc7b2b84c9f8ef87f0bc8541c9ec7176274bc
Alex Şuhan <asuhan@google.com>
2018-07-12 11:34:28 -0700

    Add support for aten::mm

--- test/test_xla_jit.py
+++ test/test_xla_jit.py
@@ -4,8 +4,11 @@ import torch.nn.functional as F
 from common import TestCase, run_tests
 
 
-def _xla_run(model, input):
+def _xla_run(model, input, decompose_addmm=False):
     traced_model = torch.jit.trace(input)(model)
+    if decompose_addmm:
+        fwd = traced_model._get_method('forward')
+        torch._C._jit_pass_decompose_addmm(fwd.graph)
     return torch._C._to_xla_module(traced_model)(input)
 
 
@@ -143,11 +146,12 @@ class TestLinear(TestCase):
             def forward(self, x):
                 return self.linear(x)
 
-        x = torch.rand(4, 2)
-        model = XlaLinear()
-        out = _xla_run(model, x)
-        expected = model(x)
-        self.assertEqual(out.data, expected.data)
+        for decompose_addmm in [False, True]:
+            x = torch.rand(4, 2)
+            model = XlaLinear()
+            out = _xla_run(model, x, decompose_addmm)
+            expected = model(x)
+            self.assertEqual(out.data, expected.data)
 
 
 class TestConv(TestCase):
