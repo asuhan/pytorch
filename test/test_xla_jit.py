84d212aa6bdff2f055a864390cb1421afbad28fc
Soumith Chintala <soumith@fb.com>
2018-07-16 08:25:22 -0600

    wip log_softmax

--- test/test_xla_jit.py
+++ test/test_xla_jit.py
@@ -256,25 +256,25 @@ class TestBatchNorm(TestCase):
         self.assertEqual(out.data, expected.data)
 
 
+class XlaMNIST(nn.Module):
+    def __init__(self):
+        super(XlaMNIST, self).__init__()
+        # self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        # self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        # self.fc1 = nn.Linear(320, 50)
+        # self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = x * x
+        # x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        # x = F.relu(F.max_pool2d(self.conv2(x), 2))
+        # x = x.view(-1, 320)
+        # x = F.relu(self.fc1(x))
+        # x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
 class TestMNIST(TestCase):
     def test(self):
-
-        class XlaMNIST(nn.Module):
-            def __init__(self):
-                super(XlaMNIST, self).__init__()
-                self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-                self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-                self.fc1 = nn.Linear(320, 50)
-                self.fc2 = nn.Linear(50, 10)
-
-            def forward(self, x):
-                x = F.relu(F.max_pool2d(self.conv1(x), 2))
-                x = F.relu(F.max_pool2d(self.conv2(x), 2))
-                x = x.view(-1, 320)
-                x = F.relu(self.fc1(x))
-                x = self.fc2(x)
-                return F.log_softmax(x, dim=1)
-
         batch_size = 32
         x = torch.randn(batch_size, 1, 28, 28)
         model = XlaMNIST()
@@ -421,6 +421,11 @@ class TestGradients(TestCase):
                             model = nn.BatchNorm2d(chans, eps=eps)
                             inputs = [torch.randn(4, chans, 28, 28, requires_grad=True)]
                             self.checkGrad(model, inputs, xla=True)
+
+    def test_mnist(self):
+        model = XlaMNIST()
+        inputs = [torch.randn(4, 1, 28, 28, requires_grad=True)]
+        self.checkGrad(model, inputs, xla=False)
 
 
 if __name__ == '__main__':
