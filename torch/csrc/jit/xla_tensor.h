8e48e000f260df7ca8f9f3a74c869641dbfdde1b
Alex Şuhan <asuhan@google.com>
2018-08-06 16:19:37 -0700

    Make XLATensor tensor operations lazy

--- torch/csrc/jit/xla_tensor.h
+++ torch/csrc/jit/xla_tensor.h
@@ -23,16 +23,30 @@ class XLATensor : public std::enable_shared_from_this<XLATensor> {
   xla::Shape shape() const;
   xla::GlobalData* data() const;
 
-  void add_(const XLATensor& other, const at::Scalar& alpha);
+  // Basic tensor operations used by the optimizers.
+  void add_(XLATensor& other, const at::Scalar& alpha);
   void mul_(const XLATensor& other);
 
+  // Applies the queue of operations in preparation for using the data.
+  void applyOps();
+
+  // Applies the queue of operations for a list of tensors.
+  static void applyOpsMulti(
+      const std::vector<std::shared_ptr<XLATensor>>& tensors);
+
  private:
+  void resetOperationsState();
+
   std::unique_ptr<xla::GlobalData> data_;
   xla::Shape shape_;
   xla::PrimitiveType dtype_; // naming dtype for consistency with at::Tensor
   bool requires_grad_;
   /* std::shared_ptr<xla::XlaComputation> grad_fn; */
   std::shared_ptr<XLATensor> grad_;
+  // Keeps track of operations applied so far.
+  at::optional<xla::XlaOp> operations_;
+  xla::XlaBuilder b_;
+  std::vector<xla::GlobalData*> operations_params_;
 };
 
 } // namespace jit
